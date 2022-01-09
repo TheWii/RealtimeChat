48540f95a90f2f7f4a3c7ba1d9bc70f6accfae39
TheWii <67249660+TheWii@users.noreply.github.com>
2022-01-09 00:12:02 -0300

    Add Home Button component

---
+++ app/src/components/nav/HomeButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <li class="item">
+        <router-link to="/"
+          :title="'Navigate back to home page'"
+        >
+            <img src="../../images/nav-home.svg" alt="Home">
+            <span>Home</span>
+        </router-link>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'HomeButton',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/header/nav.scss";
+</style>
